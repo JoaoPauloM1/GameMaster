a55ac527d59bec9ab931d6e5fc6563b76b85c2ae
João Paulo Maciel Borges <joaopaulomacielborges@gmail.com>
2025-03-07 19:54:00 -0300

    render deploy

--- setup/urls.py
+++ setup/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import index  # Importando a view do index
+from .views import index 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
